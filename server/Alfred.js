7280d13d38835fad41bb56508a2619891cd5185b
headStyleColorRed <headStyleColorRed@gmail.com>
2021-08-16 14:32:54 +0200

    Created Alfred

---
+++ server/Alfred.js
@@ -0,0 +1,17 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+function sendMessage(message, botId, chatId){
+  const bot = new TelegramBot(botId, { polling: false });
+  
+  try {
+    bot.sendMessage(chatId, message)
+  } catch {
+    return 'Something went wrong when trying to send a Telegram notification'
+  }
+
+  return "Success sending Telegram notification"
+}
+
+module.exports = {
+  sendMessage,
+}
---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express();
+const puerto = 9001;
+const Cors = require("cors")
+require('dotenv').config()
+
+// Modules
+const Alfred = require("./Alfred.js")
+const ValidationManager = require("./tools/validation.js")
+
+// Middlewares
+app.use(Cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+// Open port
+app.listen(puerto, () => console.log("Listening port " + puerto))
+
+// ++++++++++++++++ HTTP METHODS +++++++++++++++++++ //
+
+app.get("/", (req, res) => {
+	res.send("E-skeleton-telegram server is up and running! :D")
+})
+
+app.post("/send_message", async (req, res) => {
+  let body = req.body
+
+  let validationResult = ValidationManager.validateDataFields( body, ["message", "botId", "chatId"], "sending bot message to chat");
+    if (validationResult.isError)
+        return res.status(200).send({ code: validationResult.error, status: validationResult.message });
+
+    let result = Alfred.sendMessage(body.message, body.botId, body.chatId)
+
+    res.send(result)
+})
+
+module.exports = app
